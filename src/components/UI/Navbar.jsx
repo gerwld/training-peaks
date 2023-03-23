95b4bb67fc8dbbd12f68f5f5f139430f658455f9
P Jaworski <pjaworski.dev@gmail.com>
2023-03-23 16:31:14 +0100

    Added autoprefixer, etc

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { ModalAuth } from "@/components";
+import { NavLink } from "react-router-dom";
 
-const Navbar = ({authName}) => (
-   <div className="header_content line_sect">
-    <a href="/" className="navbar-l1_logo">Training App</a>
-    <div className="header_group">
-     <ModalAuth authName={authName} />
-    </div>
-   </div>
- );
+const Navbar = ({ authName }) => (
+ <div className="header_content line_sect">
+  <NavLink href="/" className="navbar-l1_logo">
+   Training App
+  </NavLink>
+  <div className="header_group">
+   <ModalAuth authName={authName} />
+  </div>
+ </div>
+);
 
 export default Navbar;
