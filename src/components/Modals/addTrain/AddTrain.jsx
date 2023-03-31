af78e8a9ed3f11912a48fabad389cab2ee7969b7
P Jaworski <pjaworski.dev@gmail.com>
2023-03-31 20:37:46 +0200

    Added popup

--- src/components/Modals/addTrain/AddTrain.jsx
+++ src/components/Modals/addTrain/AddTrain.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import AddTrainForm from './AddTrainForm'
+import React from "react";
+import AddTrainForm from "./AddTrainForm";
 
-const AddTrain = () => {
-  const onAddTrain = (data) => {
+const AddTrain = ({ isCreateMode = true, toggleNew }) => {
+ const onAddTrain = (data) => {};
+ return (
+  <div className={`modal modal_createnew ${isCreateMode ? "modal_open" : "modal_close"}`}>
+   <div className="modal_content">
+    <h1 className="modal_title">Add Training</h1>
+    <AddTrainForm onAddTrain={onAddTrain} />
+    <button onClick={toggleNew} className="btn_close">close</button>
+   </div>
+  </div>
+ );
+};
 
-  }
-  return (
-    <div className="addtrain">
-      <div className="addtrain__content">
-        <AddTrainForm onAddTrain={onAddTrain}/>
-      </div>
-    </div>
-  )
-}
-
-export default AddTrain
+export default AddTrain;
