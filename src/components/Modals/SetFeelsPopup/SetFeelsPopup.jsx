72af271b4768f010cdcf5bf7edb9d7b20091d145
P Jaworski <pjaworski.dev@gmail.com>
2023-04-03 17:59:59 +0200

    Added feels date

--- src/components/Modals/SetFeelsPopup/SetFeelsPopup.jsx
+++ src/components/Modals/SetFeelsPopup/SetFeelsPopup.jsx
@@ -1,20 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import SetFeelsForm from "./SetFeelsForm"
+import epochConvert from "utils/epochConvert"
 
 const SetFeelsPopup = ({ isFeelsMode, selectedDate, currentFeelsObj }) => {
+ const [newDate, setDate] = useState(null)
  const d = useDispatch()
  const onAddTrain = (data) => {
-  onCloseFeels();
+  onCloseFeels()
  }
 
  const onCloseFeels = () => {
   d({ type: "CLOSE_FEELSMODE" })
  }
+
+ useEffect(() => {
+  if (selectedDate) setDate(selectedDate)
+ }, [selectedDate])
+
  return (
   <div className={`modal modal_createnew ${isFeelsMode ? "modal_open" : "modal_close"}`}>
    <div className="modal_content">
-    <h1 className="modal_title">Add Feel</h1>
+    <h1 className="modal_title">Add Feel ({epochConvert(newDate, true)})</h1>
     <SetFeelsForm {...{ onAddTrain, selectedDate }} />
     <button onClick={onCloseFeels} className="btn_close">
      close
