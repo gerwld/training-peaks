a9a445bab60b6c0220f53d47d7034f9553ae9a15
P Jaworski <pjaworski.dev@gmail.com>
2023-04-07 15:48:12 +0200

    Fixed overflow

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react"
+import React from "react"
 import MainPageCalendar from "./calendar/Calendar"
 import { useDispatch, useSelector } from "react-redux"
 import { SecLoader100 } from "@/components"
--- src/pages/main/calendar/DayCellMount.jsx
+++ src/pages/main/calendar/DayCellMount.jsx
@@ -1,24 +1,32 @@
-import React from "react"
+import React, { useLayoutEffect } from "react"
 import findFeelByDate from "../../../utils/findFeelByDate"
 import { Provider } from "react-redux"
 import store from "../../../redux/store"
 import DayFeel from "./DayFeel"
 
 const DayCellMount = ({ mountData, feelsArray }) => {
-  const dayCellRef = React.useRef()
+ const dayCellRef = React.useRef()
  const dayFeel = findFeelByDate(mountData.date, feelsArray)
-
- if (dayFeel) {
-  mountData.el.querySelector(".fc-daygrid-day-frame").style.minHeight = `calc(100% - 152px)`
-  return <DayFeel {...{ dayFeel, date: mountData.date }} />
- } 
+ const daygrid = mountData?.el?.querySelector(".fc-daygrid-day-frame");
  
+
+ useLayoutEffect(() => {
+  const cellHeight = dayCellRef?.current?.offsetHeight;
+  if(daygrid) {
+    daygrid.style.minHeight = cellHeight > 5 ? `calc(100% - ${cellHeight + 5}px)` : '100%';
+  } 
+ }, [daygrid])
+
+ return (
+  <div className="dayc_mount" ref={dayCellRef}>
+   {dayFeel &&
+     <DayFeel {...{ dayFeel, date: mountData.date }} />}
+  </div>
+ )
 }
 
-
 export default (props) => (
-   <Provider store={store}>
-    <DayCellMount {...props} />
-   </Provider>
- )
- 
+ <Provider store={store}>
+  <DayCellMount {...props} />
+ </Provider>
+)
